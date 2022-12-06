290fd07c0db6f618291940df466f5a98cae96088
ZoontS <89505339+ZoontS@users.noreply.github.com>
2022-12-06 11:04:01 +0700

    Update HomePage.vue

--- meide-mech-frontend/src/views/HomePage.vue
+++ meide-mech-frontend/src/views/HomePage.vue
@@ -17,7 +17,7 @@
         </div>
       </nav>
       
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light py-3 montserrat" style="font-size: 14px; font-weight: 600;">
         <a class="navbar-brand d-md-inline d-lg-none" href="#">
           <img src="assets/images/logo-new.png" style="width: 50px;">
         </a>
@@ -26,33 +26,25 @@
           <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
+        <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+          <ul class="navbar-nav" style="margin-right: 900px;">
             <li class="nav-item active">
-              <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">HOME</a>
               <div class="dropdown-menu">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
+                <a class="dropdown-item" href="#">HOME 1</a>
+                <a class="dropdown-item" href="#">HOME 2</a>
               </div>
             </li>
-            <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">ABOUT US</a>
+            </li>
+            <li class="nav-item dropdown active">
+              <a class="nav-link" href="#">PRODUCT</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="$">CONTACT US</a>
             </li>
           </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
         </div>
       </nav>
 
