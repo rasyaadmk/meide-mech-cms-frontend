e13b56e38b86878eac24f4206f24fafe5ea26c13
ZoontS <89505339+ZoontS@users.noreply.github.com>
2022-12-11 12:12:42 +0700

    Create Carousel.vue

---
+++ meide-mech-frontend/src/views/Carousel.vue
@@ -0,0 +1,55 @@
+<template>
+    <ion-page>
+        <ion-content>
+            
+        </ion-content>
+    </ion-page>
+</template>
+  
+<script lang="ts">
+import {
+    IonContent,
+    IonPage
+}
+    from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Carousel',
+    components: {
+        IonContent,
+        IonPage
+    }
+});
+</script>
+  
+<style scoped>
+#container {
+    text-align: center;
+
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+}
+
+#container strong {
+    font-size: 20px;
+    line-height: 26px;
+}
+
+#container p {
+    font-size: 16px;
+    line-height: 22px;
+
+    color: #8c8c8c;
+
+    margin: 0;
+}
+
+#container a {
+    text-decoration: none;
+}
+</style>
+  
